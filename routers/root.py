6f0d47af7ad4d22eba6c65412996ffc96ee1a25f
daehyeoni <daehyeoni3333@gmail.com>
2023-08-24 12:56:01 +0900

    fix: root endpoint docs

--- routers/root.py
+++ routers/root.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
 
 router = APIRouter()
 
 
-@router.get("/")
-async def index(request: Request):
+@router.get(
+    "/",
+    responses={
+        200: {
+            "description": "Success",
+            "content": {"application/json": {"example": {"message": "Hello World!"}}},
+        }
+    },
+)
+async def index():
     return {"message": "Hello World!"}
