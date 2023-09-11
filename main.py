29481b2f69b5e144dc5b1772b392da72d7a5bce9
daehyeoni <daehyeoni3333@gmail.com>
2023-09-11 06:40:58 +0000

    Running on Linux Configuration

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 from routers import root
 from routers.naver import news
+import uvicorn
 
 app = FastAPI()
 app.include_router(root.router)
 app.include_router(news.router, prefix="/naver", tags=["naver"])
+
+def start():
+    uvicorn.run("main:app", host="127.0.0.1", port=3300)
