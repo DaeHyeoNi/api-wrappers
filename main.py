435d0333975ae751592cfe4df2caa4e99433c88d
daehyeoni <daehyeoni3333@gmail.com>
2023-09-11 15:43:02 +0900

    Fix Lint

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ app = FastAPI()
 app.include_router(root.router)
 app.include_router(news.router, prefix="/naver", tags=["naver"])
 
+
 def start():
     uvicorn.run("main:app", host="127.0.0.1", port=3300)
